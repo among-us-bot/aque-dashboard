8daf7d1005451cbbccc0a6a44238c288103bbc2f
TAG-Epic <admin@itzepic.net>
2020-11-14 22:36:34 +0100

    Feat: Clean it up!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
+  <NavBar/>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    NavBar,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
